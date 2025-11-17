fa4928b692d38d1a7d6cf69b52765edd9c8f883a
Josinosle <137291379+Josinosle@users.noreply.github.com>
2025-11-17 14:22:43 +0000

    fix examples to show second barrier

--- README.md
+++ README.md
@@ -107,6 +107,7 @@ ax.set_xlim(0,20)
 Barrier1 = bl.gaussian(8,0.5,4)
 Barrier2 = bl.gaussian(9,0.6,6)
 Barrier1.draw(ax)
+Barrier2.draw(ax)
 
 wavefunction = wkb.Wavefunction(1,   #constant
                                 3,   #energy
--- example.py
+++ example.py
@@ -5,29 +5,16 @@ import wkblib as wkb
 fig = plt.figure()
 ax = fig.add_subplot(111)
 
-"""
-Barrier1 = bl.cube(1,0.5,10)
-ax = Barrier1.draw(ax)
-Barrier2 = bl.triangle(1.2,0.5,9)
-ax = Barrier2.draw(ax)
-
-Barrier3 = bl.gaussian(8,0.5,11)
-ax = Barrier3.draw(ax)
-#ax.set_ylim(0,1)
-"""
-
 ax.set_xlim(0,20)
 
-Barrier = bl.gaussian(
-    8,
-    0.5,
-    4
-)
-Barrier.draw(ax)
+Barrier1 = bl.gaussian(8,0.5,4)
+Barrier2 = bl.gaussian(9,0.6,6)
+Barrier1.draw(ax)
+Barrier2.draw(ax)
 
-wavefunction = wkb.Wavefunction(1,
-                                2,
-                                1,
-                                [Barrier])
+wavefunction = wkb.Wavefunction(1,   #constant
+                                3,   #energy
+                                1,   #mass
+                                [Barrier1,Barrier2])   #barriers
 
 wavefunction.plot(ax,fig,save=False,animated=False)
